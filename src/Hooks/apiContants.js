86f8c5673b26649f70748b97ace886764a16357e
davedigi <davedigi@tiscali.it>
2020-10-25 23:21:43 +0100

    defining constant fro clock

--- src/Hooks/apiContants.js
+++ src/Hooks/apiContants.js
@@ -2,17 +2,20 @@
 export const APP_BASE_URL = "http://localhost"
 export const APP_API_URL = "http://localhost"
 export const APP_API_PORT = 4000
-export const APP_API_CLOCK_URL = "http://localhost"
-// export const APP_API_CLOCK_URL = "10.100.1.100"  in Sintel Loreggia ADM
-export const APP_API_CLOCK_PORT = 5000
 export const ACCESS_TOKEN_NAME = "login_access_token"
 export const APP_TITLE = "Auction Sales Web App"
 export const APP_ENV = "develop"
 
+// export const APP_MIRROR_CLOCK_URL = "http://10.100.1.109:9090"  // in Sintel Loreggia
+export const APP_MIRROR_CLOCK_URL = "http://localhost:9090"  // in Sintel Loreggia
+/*
+export const APP_API_CLOCK_URL = "http://localhost"
+export const APP_API_CLOCK_PORT = 5000
 
-// direct digital watch services
-export const PROC_API_PDD_START = "/api/clock/start"        // start watch method=POST
-export const PROC_API_PDD_STOP = "/api/clock/stop"          // stop watch method=GET
-export const PROC_API_GDD_DISPLAY = "/api/clock/display"    // display info on watch method=POST
+// direct digital clock services
+export const PROC_API_PDD_START = "/api/clock/start"        // start clock method=POST
+export const PROC_API_PDD_STOP = "/api/clock/stop"          // stop clock method=GET
+export const PROC_API_GDD_DISPLAY = "/api/clock/display"    // display info on clock method=POST 
+*/
 
 
